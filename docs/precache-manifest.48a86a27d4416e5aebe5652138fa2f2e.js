6e286c921172c7eed9521c942a52554201cc0535
Avi Bagla <avibagla@gmail.com>
2018-11-27 23:20:44 -0800

    website

---
+++ docs/precache-manifest.48a86a27d4416e5aebe5652138fa2f2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d362008d029cea57e71bd2373aea032d",
+    "url": "/static/media/sample.d362008d.mp3"
+  },
+  {
+    "revision": "436acc6af46e085a3fb4fc61a79b864a",
+    "url": "/static/media/logo.436acc6a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e73d102f21a10eded60d",
+    "url": "/static/js/main.e73d102f.chunk.js"
+  },
+  {
+    "revision": "e59f22e88b45af89206d",
+    "url": "/static/js/1.e59f22e8.chunk.js"
+  },
+  {
+    "revision": "e73d102f21a10eded60d",
+    "url": "/static/css/main.0949f727.chunk.css"
+  },
+  {
+    "revision": "e59f22e88b45af89206d",
+    "url": "/static/css/1.7f97c9f2.chunk.css"
+  },
+  {
+    "revision": "f87f045693b891693619ad6ece888a23",
+    "url": "/index.html"
+  }
+];
